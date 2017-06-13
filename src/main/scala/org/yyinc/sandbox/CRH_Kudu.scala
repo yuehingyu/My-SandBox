e982b2b14b1961af606758950873b9f50ae6083e
Yuehing Yu <yu.yuehing@gmail.com>
2017-06-13 18:52:05 -0400

    Add create & load comparision

--- src/main/scala/org/yyinc/sandbox/CRH_Kudu.scala
+++ src/main/scala/org/yyinc/sandbox/CRH_Kudu.scala
@@ -46,6 +46,10 @@ object CRH_Kudu {
     
     // this column does not shown when using spark-shell complaintDF.drop("_corrupt_record")
     
+    
+    complaintDF.drop("_corrupt_record")
+    
+    
     complaintDF.printSchema()
 
     complaintDF.createOrReplaceTempView("my_temp_table_view")
@@ -55,7 +59,13 @@ object CRH_Kudu {
     spark.sql("DROP TABLE IF EXISTS customer_complaint") 
     
     
+    // create table and insert at the same time.. lazy way
+    
     spark.sql("CREATE TABLE customer_complaint STORED AS ORC  AS SELECT * from my_temp_table_view")
+    
+    
+    
+    spark.sql("INSERT INTO customer_complaint_v2 SELECT company,complaint_id,timely_response from my_temp_table_view")
     
     
     spark.sql("select * from customer_complaint").show()
@@ -72,8 +82,50 @@ object CRH_Kudu {
     
     printf("Total row : "+summaryResult.count())
     
+    
+    // create table first and insert data from the temp view table.. spark sql does not support CLUSTERED by ..
+    
+    
+    //val sqlSTMT="CREATE TABLE customer_complaint_v2 (company string, complaint_id string,timely_response string) stored as ORC"
+    
+    
+    //spark.sql("DROP TABLE IF EXISTS customer_complaint_v2")
+    
+    //spark.sql(sqlSTMT)
+    
+/*
+ * 
+ * CREATE TABLE customer_complaint_v2 (
+company                        string,
+complaint_id                   string,
+timely_response                string
+)
+CLUSTERED BY (complaint_id) INTO 2 BUCKETS STORED AS ORC
+TBLPROPERTIES ("transactional"="true",
+  "compactor.mapreduce.map.memory.mb"="2048",
+  "compactorthreshold.hive.compactor.delta.num.threshold"="4",
+  "compactorthreshold.hive.compactor.delta.pct.threshold"="0.5"
+)
 
+* */
+    
+    
+    spark.sql("INSERT INTO customer_complaint_v2 SELECT company,complaint_id,timely_response from my_temp_table_view")
+    
+    val summarySQL2="select a.company as company, a.case as case, b.case as timely_response, c.case as non_timely_response from (select company,count(*) as case from customer_complaint_v2 group by company) as a inner join (select company,count(*) as case from customer_complaint_v2 where timely_response='Yes' group by company) as b inner join (select company,count(*) as case from customer_complaint_v2 where timely_response='No' group by company) as c where a.company=b.company and a.company==c.company order by case desc"
+    
+    val summaryResult2=spark.sql(summarySQL2)
+    
+    summaryResult2.show()
 
+    printf("Total row : "+summaryResult2.count())
+
+    val count=spark.sql("select count(*) from customer_complaint_v2").head().getLong(0)
+    
+    printf("Total rows : "+count)
    
+    
+    
+      
   }
 }
